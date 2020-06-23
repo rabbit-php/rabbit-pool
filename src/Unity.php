ae4a13a46ee6b4156528a5fa165894c5082f336d
albert <amuluowin@163.com>
2020-06-23 13:56:30 +0800

    add Unity retry

--- src/Unity.php
+++ src/Unity.php
@@ -30,18 +30,7 @@ class Unity
      */
     public function __invoke(callable $function)
     {
-        $client = $this->pool->get();
-        if ($client instanceof IUnity) {
-            $client = $client->build();
-        }
-        try {
-            return call_user_func($function, $client);
-        } catch (Throwable $exception) {
-            App::error($exception->getMessage());
-            throw $exception;
-        } finally {
-            $this->pool->release($client);
-        }
+        return $this->realCall($function);
     }
 
     /**
@@ -52,20 +41,43 @@ class Unity
      */
     public function __call($name, $arguments)
     {
-        $client = $this->pool->get();
-        if ($client instanceof IUnity) {
-            $client = $client->build();
-        }
-        try {
-            if (is_callable([$client, $name])) {
-                return $client->$name(...$arguments);
+        return $this->realCall($name, $arguments);
+    }
+
+    /**
+     * @param $call
+     * @param array $params
+     * @return mixed
+     * @throws NotSupportedException
+     * @throws \rabbit\core\Exception
+     */
+    protected function realCall($call, array $params = [])
+    {
+        $retrys = $this->pool->getPoolConfig()->getMaxRetry();
+        $retrys = $retrys > 0 ? $retrys : 1;
+        while ($retrys--) {
+            $client = $this->pool->get();
+            if ($client instanceof IUnity) {
+                $client = $client->build();
             }
-            throw new NotSupportedException(get_class($client) . " has no method $name");
-        } catch (Throwable $exception) {
-            App::error($exception->getMessage());
-            throw $exception;
-        } finally {
-            $this->pool->release($client);
+            try {
+                if (is_string($call) && is_callable([$client, $call])) {
+                    $result = $client->$call(...$params);
+                } elseif (is_callable($call)) {
+                    $result = call_user_func($call, $client);
+                } else {
+                    throw new NotSupportedException(get_class($client) . " has no method $name");
+                }
+                $this->pool->release($client);
+                return $result;
+            } catch (Throwable $exception) {
+                $this->pool->sub();
+                if ($retrys === 0) {
+                    App::error($exception->getMessage());
+                    throw $exception;
+                }
+                App::warning($exception->getMessage() . ' & retry!');
+            }
         }
     }
 }
